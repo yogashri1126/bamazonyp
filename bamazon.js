c7ff934b2f2f74e049f74e0187b96b04b308d021
yogashri1126 <ypradhan.pge@texas.edu>
2017-09-21 18:17:37 -0500

    prototype references within the update function

--- bamazon.js
+++ bamazon.js
@@ -1,102 +1,132 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var temp = 0;
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "MyNewPass",
-  database: "bamazon"
+    // Your password
+    password: "MyNewPass",
+    database: "bamazon"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+
 
 });
 
 connection.query("SELECT * FROM products", function(err, results) {
-if (err) throw err;
-console.log(results)})
+    if (err) throw err;
+    console.log(results)
+})
 
 start();
 
 // function which prompts the user for what action they should take
 function start() {
-  inquirer
-    .prompt({
-      name: "buymore",
-      type: "rawlist",
-      message: "Would you like to buy more things?",
-      choices: ["YES", "NAH"]
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.buymore.toUpperCase() === "YES") {
-        purchase();
-      }
-      else {
-        console.log("You're missing out!!!")
-      }
-    });
+    inquirer
+        .prompt({
+            name: "buymore",
+            type: "rawlist",
+            message: "Would you like to buy more things?",
+            choices: ["YES", "NAH"]
+        })
+        .then(function(answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.buymore.toUpperCase() === "YES") {
+                purchase();
+            } else {
+                console.log("You're missing out!!!")
+            }
+        });
 }
 
 // function to handle posting new items up for auction
 function purchase() {
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: "id",
-        type: "input",
-        message: "What is the product ID of which you want to buy?"
-      },
-      {
-        name: "unitz",
-        type: "input",
-        message: "How many units?"
-      }
-      // {
-      //   name: "startingBid",
-      //   type: "input",
-      //   message: "What would you like your starting bid to be?",
-      //   validate: function(value) {
-      //     if (isNaN(value) === false) {
-      //       return true;
-      //     }
-      //     return false;
-      //   }
-      // }
-    ])
-    .then(function(answer) {
-      connection.query(
-        "SELECT * FROM products SET ?",
-        {
-          //need to do a for loop to go through 
-          //primary key to check the name of the product
-          //and if there are enough units
-          //if both conditions are met, SUBTRACT number of units and
-          //console log "Your ordered...successfully!" and DISPLAY table
-          //if one or more conditions are not met, then console log 
-          //"sorry, can't make this transaction!"
-          item_id: answer.id,
-          stock_quantity: answer.unitz
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("Your ordered product is processed successfully!");
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([{
+                name: "id",
+                type: "input",
+                message: "What is the product ID of which you want to buy?"
+            }, {
+                name: "unitz",
+                type: "input",
+                message: "How many units?"
+            }
+            // {
+            //   name: "startingBid",
+            //   type: "input",
+            //   message: "What would you like your starting bid to be?",
+            //   validate: function(value) {
+            //     if (isNaN(value) === false) {
+            //       return true;
+            //     }
+            //     return false;
+            //   }
+            // }
+        ])
+        .then(function(answers) {
+            connection.query(
+                "SELECT item_id FROM products WHERE stock_quantity>=?", answers.unitz,
+                function(res, err) {
 
-          start();
+                    if (err) throw err;
+                    //need to do a for loop to go through 
+                    //primary key to check the name of the product
+                    //and if there are enough units
+                    //if both conditions are met, SUBTRACT number of units and
+                    //console log "Your ordered...successfully!" and DISPLAY table
+                    //if one or more conditions are not met, then console log 
+                    //"sorry, can't make this transaction!"
+                    for (i = 0; i < res.length; i++) {
+                        if (answer.id === res[i].item_id && answer.unitz <= res[i].stock_quantity) {
+                            console.log("Your ordered product is processed successfully!")
+                            temp = res[i].stock_quantity - answer.unitz
+                            update(answers)
+
+                            start();
+                        } else {
+                            console.log("sorry, can't make this transaction!")
+                        }
+                    }
+
+
+
+
+                }
+
+            );
+        });
+}
+
+function update(answers) {
+
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?", [{
+            stock_quantity: temp
+        }, {
+            item_id: answers.id
+        }],
+        function(err, res) {
+            console.log(res.affectedRows + " products updated!\n");
+            // Call deleteProduct AFTER the UPDATE completes
+
         }
-      );
-    });
+    );
+    console.log(query.sql)
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(json.stringify(results))
+    })
 }
 
 // function bidAuction() {
